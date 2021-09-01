78effb88309e2d4208cc4312a22e231e17c1eb4b
Septiana Yoga <septianayoga30@gmail.com>
2021-09-01 14:07:22 +0700

    adding styles for cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import CardItem from './CardItem'
+import React from "react";
+import CardItem from "./CardItem";
+import "./Cards.css";
 
 function Cards() {
   return (
@@ -13,7 +14,7 @@ function Cards() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
