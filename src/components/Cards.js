b518bb56d3d00fe70b06b5f4d92473112edd6f5d
Septiana Yoga <septianayoga30@gmail.com>
2021-09-01 14:14:38 +0700

    fixing styles for cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,9 +8,40 @@ function Cards() {
       <h1>Check out these EPIC Destinations!</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
-          <div className="cards__items">
-            <CardItem />
-          </div>
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-9.jpg"
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Travel through the Islands of Bali in a Private Cruise"
+              label="Cruise"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-9.jpg"
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Travel through the Islands of Bali in a Private Cruise"
+              label="Cruise"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Travel through the Islands of Bali in a Private Cruise"
+              label="Cruise"
+              path="/services"
+            />
+          </ul>
         </div>
       </div>
     </div>
